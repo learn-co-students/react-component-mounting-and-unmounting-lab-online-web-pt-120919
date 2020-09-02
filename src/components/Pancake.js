6b60b83029120fadb27bc4ec5adf7149432aede1
Mely07 <damely.tineo07@gmail.com>
2020-09-02 17:30:22 -0400

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 class Pancake extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
       timeCooked: 0,
